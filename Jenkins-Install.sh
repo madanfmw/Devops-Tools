7cb7a0d5f2d1141a5d90f3eeef1a0800b1cebccf
Madan Mohan <52009393+madanfmw@users.noreply.github.com>
2024-12-04 09:45:34 +0530

    Update Jenkins-Install.sh

--- Jenkins-Install.sh
+++ Jenkins-Install.sh
@@ -6,8 +6,7 @@ sudo yum update -y
 
 # Install Java (Amazon Linux 2 uses Amazon Corretto 8/11 as the default JDK)
 echo "Installing Java..."
-sudo amazon-linux-extras enable corretto8
-sudo yum install java-1.8.0-amazon-corretto -y
+sudo dnf install java-17-amazon-corretto -y
 
 # Verify Java installation
 echo "Verifying Java installation..."
